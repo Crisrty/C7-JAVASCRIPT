8d9ebcd7ed056bf7d5862f4cad1247a356839973
Cristina Reyes <cristinabm.rs@gmail.com>
2022-06-04 11:28:23 -0500

    arreglos declaracion

--- main.js
+++ main.js
@@ -4,4 +4,24 @@ console.log(arreglo);
 
 arreglo.unshift("elemento") //Agregar elementos al incio
 console.log(arreglo);
+console.log(arreglo.length);//Muestra el tamaño del arreglo
+
+var num1 = 5;
+var num2 = 3;
+var num3 = num1;
+
+num3=100;
+console.log(num3+num1);
+console.log(num2);
+
+var arreglo1 = [1, 2, 3];
+var arreglo2 = arreglo1;
+console.log(arreglo2);
+arreglo2.push(4);
+console.log(arreglo2);
+console.log(arreglo1);
+
+
+
+
 
